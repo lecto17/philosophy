50c6acf7496c6fc07e7fc63d1ccf761d471c1559
lecto17 <khwoo_rpsit@naver.com>
2021-01-12 20:13:42 +0900

    로그인 화면 작성 중

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="login">
+        <div class="top">
+            <img class="nav-img" src="../assets/logo.png"/>
+        </div>
+        <div class="input-info">
+            <div>
+    		    <div id="google-signin-btn"></div>
+  	        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+	mounted: function() {
+    	// 로그인에 사용할 버튼 id를 입력해주세요
+		gapi.signin2.render("google-signin-btn", {
+      		onsuccess: this.onSignIn, 
+    	});
+    },
+    methods:{
+    	onSignIn(googleUser){
+        	// 로그인한 유저 정보를 보여줍니다.
+        	console.log(googleUser)
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.nav-img { height: 20vh; border: 1px solid black;}
+
+</style>
